6c5b68603d9ab722964ab59a03d4d0689b4bbe6a
熙然 <970852638@qq.com>
2024-07-30 21:51:27 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -29,7 +29,9 @@ const BLOG = {
   ogImageGenerateHost: 'og-zl.vercel.app', // The link to generate OG image, don't end with a slash
   defaultCover: '/cover.jpg',
   socialLink: {
+    twitter: '',
     github: 'https://github.com/xixiranran',
+    telegram: 'https://t.me/zuolan'
   },
   seo: {
     keywords: ['熙然博客', 'xiranblog', '熙然', 'xiran', 'xixiranran', '博客', 'Blog'],
