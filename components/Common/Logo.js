4a3c442b942605cf02da7f6c066c5ce41dd15d59
熙然 <970852638@qq.com>
2024-08-01 23:06:25 +0800

    Update Logo.js

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -2,7 +2,7 @@
 import * as React from 'react'
 
 const Logo = (props) => (
-  <img src="https://img.xixiranran.com/2024/20240730_1.png" width="30" height="30" />
+  <img src="https://img.xixiranran.com/2024/logo.png" width="30" height="30" />
 )
 
 export default Logo
