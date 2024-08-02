d7612a1ec09eaa117d64b01c33caac43591e0bde
熙然 <970852638@qq.com>
2024-08-02 21:03:06 +0800

    Update htmlrewrite.js

--- pages/api/htmlrewrite.js
+++ pages/api/htmlrewrite.js
@@ -85,12 +85,12 @@ module.exports = async (req, res) => {
   const modifyResText = originResText
     .replace('<meta name="robots" content="noindex">', '')
     .replace(
-      '<link rel="icon" href="/share/static/favicon.ico">',
-      '<link rel="icon" href="/favicon.svg">'
+      '<link rel="icon" href="/share/static/logo.svg">',
+      '<link rel="icon" href="/logo.svg">'
     )
     .replace(
       '<link rel="apple-touch-icon" href="/share/static/logo-192.png">',
-      '<link rel="apple-touch-icon" href="/favicon.png">'
+      '<link rel="apple-touch-icon" href="/logo.svg">'
     )
     .replace(
       '<link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,300i,400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet"><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">',
